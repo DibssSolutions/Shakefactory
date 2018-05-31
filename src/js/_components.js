75bb9cda0d2b9a602ff0960550a8bc465adba829
stasumanskiy <stasumanskiy@gmail.com>
2018-05-31 13:25:21 +0300

    styling adaptive menu

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
 import './components/_header';
 import './components/_nav';
 import './components/_sliderProduct.js';
+import './components/_fancybox3';
---
+++ src/js/components/_fancybox3.js
@@ -0,0 +1,14 @@
+import fancybox from '@fancyapps/fancybox';
+
+$('[data-fancybox]').fancybox({
+  vimeo : {
+    color : 'f00'
+  },
+  media : {
+    youtube : {
+      params : {
+        autoplay : 0
+      }
+    }
+  }
+});
