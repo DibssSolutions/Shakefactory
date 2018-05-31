7ff82583a886a2a337ef92f78184ee6c2c38df33
Lusjen <lusjenh@gmail.com>
2018-05-31 18:42:32 +0300

    chakers

--- src/js/components/_isotope-layout.js
+++ src/js/components/_isotope-layout.js
@@ -6,13 +6,16 @@ console.log('hello');
 var iso = new Isotope( '.grid', {
   // options
   itemSelector: '.grid__col',
-  layoutMode: 'fitRows'
+  masonry: {
+    gutter: 70,
+    horizontalOrder: true
+  }
 });
 
 // bind filter button click
 $('.js-filters').on( 'click', 'button', function() {
   var filterValue = $( this ).attr('data-filter');
   // use filterFn if matches value
-  filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
+  // filterValue = filterFns[ filterValue ] || filterValue;
+  iso.arrange({ filter: filterValue });
 });
