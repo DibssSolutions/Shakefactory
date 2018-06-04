6fc459831bb432f946b069a16797b0b4f860c869
stasumanskiy <stasumanskiy@gmail.com>
2018-06-04 16:52:39 +0300

    add send.php file

--- src/js/components/_ajax.js
+++ src/js/components/_ajax.js
@@ -1,11 +1,11 @@
-// $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
-//             var form_data = $(this).serialize(); //собераем все данные из формы
-//             $.ajax({
-//             type: "POST", //Метод отправки
-//             url: "send.php", //путь до php фаила отправителя
-//             data: form_data,
-//             success: function() {
-//                    //код в этом блоке выполняется при успешной отправке сообщения
-//                    alert("Ваше сообщение отпрвлено!");
-//             });
-//     });
+
+console.log('hi');
+$('#form').submit(function() {
+  var form_data = $(this).serialize();
+  $.ajax({
+    type: 'POST',
+    url: 'send.php',
+    data: form_data,
+    success: function() {}
+  });
+});
