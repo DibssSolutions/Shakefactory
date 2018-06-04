cd89d6354d7128c8e732fe5ede26034570d74d5c
Валерий <kaktuz34@gmail.com>
2018-06-04 12:59:10 +0300

    upd

--- src/js/components/helpers/_scrollWidth.js
+++
@@ -1,6 +0,0 @@
-export default () => {
-  let documentWidth = parseInt(document.documentElement.clientWidth),
-	  windowsWidth = parseInt(window.innerWidth),
-	  scrollbarWidth = windowsWidth - documentWidth;
-  return scrollbarWidth;
-};
