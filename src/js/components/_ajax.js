4c54904dca49e352ec8ab21b8640180c5a7b91d5
stasumanskiy <stasumanskiy@gmail.com>
2018-06-05 12:00:50 +0300

    fix ajax

--- src/js/components/_ajax.js
+++ src/js/components/_ajax.js
@@ -1,5 +1,5 @@
-$('.js-form').submit(function(e) {
-  e.preventDefault();
+$('.js-form').submit(function() {
+  // e.preventDefault();
   var formData = $(this).serialize();
   $.ajax({
     type: 'POST',
@@ -7,17 +7,18 @@ $('.js-form').submit(function(e) {
     data: formData,
     success: function() {
       var sentMessage = $('.js-success');
+
+      $('form').get(0).reset();
       sentMessage.fadeIn(200);
       sentMessage.fadeOut(5000);
       console.log('Ваше сообщение отпрвлено!');
     },
     error: function() {
       // form.reset();
-      $('form').get(0).reset();
       var errorSent = $('.js-success-error');
+
       errorSent.fadeIn(200);
       errorSent.fadeOut(5000);
-
       console.log('Что-то пошло не так!');
     }
   });
