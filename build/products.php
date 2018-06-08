5b6136f3e2282db982833a06fd818b66143cfc76
stasumanskiy <stasumanskiy@gmail.com>
2018-06-08 17:34:16 +0300

    contact form (php)

--- build/products.php
+++ build/products.php
@@ -1,23 +1,43 @@
-<?
-    if ((isset($_POST['name']) && $_POST['name'] != "") && (isset($_POST['email']) && $_POST['email'] != "")) {
-            $to = 'stasumanskiy@gmail.com';
-            $subject = 'Shakefactory Products';
-            $message = '
-                    <html>
-                        <head>
-                            <title>'.$subject.'</title>
-                        </head>
-                        <body>
-                            <p>Vendor Code: '.$_POST['vendor'].'</p>
-                            <p>Name: '.$_POST['name'].'</p>
-                            <p>Email: '.$_POST['email'].'</p>                       
-                            <p>Website: '.$_POST['website'].'</p>                       
-                            <p>Order Quontity: '.$_POST['state'].'</p>                       
-                            <p>Message: '.$_POST['message'].'</p>                     
-                        </body>
-                    </html>';
-            $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-            $headers .= "From: Отправитель <from@example.com>\r\n";
-            mail($to, $subject, $message, $headers);
-    }
+<?php require_once('class.phpmailer.php');
+        
+        error_reporting(E_STRICT);
+
+        $mail = new PHPMailer();
+        $mail->SetFrom($_POST['email'], $_POST['name']);
+        $mail->AddReplyTo($_POST['email'], $_POST['name']);
+        $mail->Subject = "Request Quote";
+
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $productname = $_POST['vendor'];
+        $quantity = $_POST['state'];
+        $company = $_POST['website'];
+        $message = $_POST['message'];
+        
+        $htm = "Vendor Code : ".$productname."<br />";
+        $htm .= "Name : ".$name."<br />";
+        $htm .= "Email : ".$email."<br />";
+        $htm .= "Quantity : ".$quantity."<br />";
+        $htm .= "Company : ".$company."<br />";
+        $htm .= "Message : ".$message."<br />";
+        
+        $mail->MsgHTML($htm);
+        
+        $mail->AddAddress("info@shakerfactory.com", "Shakerfactory");
+
+        /* auto reply */
+        $mreply = new PHPMailer();      
+        $mreply->SetFrom("info@shakerfactory.com", "Shakerfactory");        
+        $mreply->AddReplyTo("info@shakerfactory.com", "Shakerfactory");     
+        $mreply->Subject = "Shakerfactory Request Received!";       
+        $mreply->MsgHTML("Thanks for your inquiry! We'll contact you shortly to learn more and answer any questions you might have.<br><br>Best Regards,<br>The Shakerfactory Team");       
+        $mreply->AddAddress($_POST['email'], $_POST['name']);
+        $mreply->Send();        
+
+        if(!$mail->Send()) {
+          echo "Error: " . $mail->ErrorInfo;
+        } else {
+          echo "Complete";
+        }
+        
 ?>
--- build/send.php
+++ build/send.php
@@ -1,21 +1,38 @@
-<?
-    if ((isset($_POST['name']) && $_POST['name'] != "") && (isset($_POST['email']) && $_POST['email'] != "")) {
-        $to = 'stasumanskiy@gmail.com';
-        $subject = 'Shakefactory';
-        $message = '
-                <html>
-                    <head>
-                        <title>'.$subject.'</title>
-                    </head>
-                    <body>
-                        <p>Name: '.$_POST['name'].'</p>
-                        <p>Email: '.$_POST['email'].'</p>                       
-                        <p>Website: '.$_POST['website'].'</p>                       
-                        <p>Message: '.$_POST['message'].'</p>                     
-                    </body>
-                </html>';
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-        $headers .= "From: Отправитель <from@example.com>\r\n";
-        mail($to, $subject, $message, $headers);
-    }  
+<?php require_once('class.phpmailer.php');
+                
+    error_reporting(E_STRICT);
+    
+    $mail = new PHPMailer();
+    $mail->SetFrom($_POST['email'], $_POST['name']);
+    $mail->AddReplyTo($_POST['email'], $_POST['name']);
+    $mail->Subject = "Contact From";
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $company = $_POST['website'];
+    $message = $_POST['message'];
+    
+    $htm = "Name : ".$name."<br />";
+    $htm .= "Email : ".$email."<br />";
+    $htm .= "Company : ".$company."<br />";
+    $htm .= "Message : ".$message."<br />";
+    
+    $mail->MsgHTML($htm);
+    
+    $mail->AddAddress("info@shakerfactory.com", "Shakerfactory");
+
+    /* auto reply */
+    $mreply = new PHPMailer();      
+    $mreply->SetFrom("info@shakerfactory.com", "Shakerfactory");        
+    $mreply->AddReplyTo("info@shakerfactory.com", "Shakerfactory");     
+    $mreply->Subject = "Shakerfactory Contact";     
+    $mreply->MsgHTML("Thanks for your inquiry, we will get back to you shortly.");      
+    $mreply->AddAddress($_POST['email'], $_POST['name']);
+    $mreply->Send();        
+
+    if(!$mail->Send()) {
+      echo "Error: " . $mail->ErrorInfo;
+    } else {
+      echo "Complete";
+    }
 ?>
